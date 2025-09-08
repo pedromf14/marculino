ae5c93f7103d826795bf07c69c535c6a1fd57520
pedromf14 <pedromarculino000@gmail.com>
2025-09-07 22:49:40 -0300

    Create perguntas.php

---
+++ perguntas.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+$perguntas = [
+    [
+        "pergunta" => "Em que ano foi lançado o primeiro PlayStation?",
+        "opcoes" => ["1990", "1994", "1996"],
+        "resposta" => 1
+    ],
+    [
+        "pergunta" => "Qual empresa desenvolveu o PlayStation?",
+        "opcoes" => ["Nintendo", "Sony", "Sega"],
+        "resposta" => 1
+    ],
+    [
+        "pergunta" => "Qual é o nome do controle clássico do PlayStation?",
+        "opcoes" => ["DualShock", "Pro Controller", "Joy-Con"],
+        "resposta" => 0
+    ],
+    [
+        "pergunta" => "Qual jogo foi o mais vendido do PlayStation 2?",
+        "opcoes" => ["GTA: San Andreas", "Gran Turismo 3", "God of War II"],
+        "resposta" => 0
+    ],
+    [
+        "pergunta" => "Qual é o serviço de assinatura online da Sony?",
+        "opcoes" => ["Xbox Live", "PlayStation Plus", "Game Pass"],
+        "resposta" => 1
+    ],
+    [
+        "pergunta" => "Qual foi o primeiro console PlayStation a ter suporte a Blu-ray?",
+        "opcoes" => ["PlayStation 2", "PlayStation 3", "PlayStation 4"],
+        "resposta" => 1
+    ],
+    [
+        "pergunta" => "Qual desses jogos é exclusivo do PlayStation?",
+        "opcoes" => ["Halo", "Uncharted", "The Legend of Zelda"],
+        "resposta" => 1
+    ]
+];
